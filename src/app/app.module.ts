50ed34e983ebe573d6e27fe209afe449bd5c2361
JOBJACK <119332782+JOBJACK@users.noreply.github.com>
2022-12-15 13:48:45 +0200

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatChipsModule } from '@angular/material/chips';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -16,6 +18,8 @@ import { HttpClientModule } from '@angular/common/http';
     MatIconModule,
     MatDividerModule,
     MatTooltipModule,
+    MatChipsModule,
+    FormsModule,
   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
